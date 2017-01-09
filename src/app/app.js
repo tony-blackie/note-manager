2abd28274606e8c1799fb9fa7a9fced67eb9ed7b
MacBook <jim.casey321@gmail.com>
2017-01-09 14:31:25 +0200

    Add rect-dom react and react-router to the project

--- src/app/app.js
+++ src/app/app.js
@@ -1,2 +1,19 @@
 import string from './string.js';
-console.log(string);
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+import { Router, Route, Link, browserHistory } from 'react-router';
+
+//render((
+//   <Router history={browserHistory}>
+//    <Route path='/' component={App} />
+//    <Route path='*' component={NoMatch} />
+//   </Router>
+//), document.getElementById('root'));
+
+export default class App extends Component {
+    render() {
+        return (
+            <div>App Component</div>
+        );
+    }
+}
