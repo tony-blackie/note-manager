fcc0d695d796f15d1b3349bcb4d3b7757ebe9f75
MacBook <jim.casey321@gmail.com>
2017-02-19 15:35:50 +0200

    Add styles for note-panel

--- src/components/NotePanel.jsx
+++ src/components/NotePanel.jsx
@@ -3,8 +3,16 @@ import React, { Component } from 'react';
 export default class NoteComponent extends Component {
     render() {
         return (
-            <div>
-                <div>Note</div>
+            <div className="note-panel">
+                <div className="note-search">
+                    <input type="text" placeholder="search" />
+                </div>
+                <div className="note">
+                    <i className="fa fa-file-text-o fa-3x"></i>
+                    <div className="note-text">
+                        <span></span>
+                    </div>
+                </div>
                 <div>Note</div>
                 <div>Note</div>
             </div>
