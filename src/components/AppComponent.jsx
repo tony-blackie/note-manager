77c3c1dccba4f9213418abcc445539018becae3b
MacBook <jim.casey321@gmail.com>
2017-02-20 00:06:38 +0200

    Add store and reducer to the app

--- src/components/AppComponent.jsx
+++ src/components/AppComponent.jsx
@@ -5,6 +5,40 @@ import ControlPanel from './ControlPanel.jsx';
 import FolderTree from './FolderTree.jsx';
 import NotePanel from './NotePanel.jsx';
 import Folder from './Folder.jsx';
+import { createStore } from 'redux';
+
+let initialState = {
+    folders: [
+        {
+            id: 0,
+            children: [],
+            isActive: false,
+            isOpen: false
+        },
+        {
+            id: 1,
+            children: [],
+            isActive: false,
+            isOpen: false
+        },
+        {
+            id: 2,
+            children: [],
+            isActive: false,
+            isOpen: false
+        }
+    ]
+};
+
+const reducer = (state, action) => {
+    if (typeof state === 'undefined') {
+        return initialState;
+    }
+
+    return state;
+};
+
+let store = createStore(reducer);
 
 
 export default class App extends Component {
