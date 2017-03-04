77f7b9ced65af0c8573c332978c9d174030f21b5
MacBook <jim.casey321@gmail.com>
2017-03-04 16:19:53 +0200

    Fix error on the server and remove redundant comments

--- server.js
+++ server.js
@@ -16,50 +16,17 @@ var connection = mysql.createConnection({
 app.get('/', () => connection.connect());
 
 app.get('/notes', function(req, res) {
-    connection.connect();
-
     connection.query('SELECT * FROM note', function (err, results, fields) {
         if (err) throw err;
 
         res.send(results);
     });
-
-    connection.end();
 });
 
 app.get('/notes/:id', (request, response) => {
     const id = request.params.id;
 
-    const dbQuery = `select * from note where id=${id}`;
-    // let name, text;
-
-    // switch(id) {
-    //     case '0':
-    //       name = 'Name 1';
-    //       text = 'This is a text of the first note';
-    //       break;
-    //     case '1':
-    //       name = 'Name 2';
-    //       text = 'Text of the second note is very different';
-    //       break;
-    //     case '2':
-    //       name = 'Name 3';
-    //       text = 'Very original text';
-    //       break;
-    //     default:
-    //       console.log('fail');
-    //       return;
-    // }
-    //
-    // response.send(
-    //     {
-    //         id: id,
-    //         name: name,
-    //         text: text
-    //     }
-    // );
-
-    connection.query(dbQuery, function (err, results, fields) {
+    connection.query(`SELECT * FROM note WHERE id=${id}`, function (err, results, fields) {
         if (err) throw err;
 
         response.send(results[0]);
