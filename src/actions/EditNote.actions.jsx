d5f08cfe4125a4ddd72b96c27147a74167e06002
MacBook <jim.casey321@gmail.com>
2017-03-05 22:18:21 +0200

    Add redirect to main page after updating the note

--- src/actions/EditNote.actions.jsx
+++ src/actions/EditNote.actions.jsx
@@ -1,8 +1,12 @@
+import { hashHistory } from 'react-router';
+
 export function updateNote(note) {
     $.ajax({
       url: `/notes/${note.id}`,
       data: JSON.stringify(note),
       contentType: 'application/json',
       type: 'PUT'
+    }).then(response => {
+        hashHistory.push('/');
     });
 }
