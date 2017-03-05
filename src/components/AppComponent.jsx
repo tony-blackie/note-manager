4a5a8cb64b2a1f8ace79063d045e03bd57fb0292
MacBook <jim.casey321@gmail.com>
2017-03-05 19:49:44 +0200

    Improve code styling

--- src/components/AppComponent.jsx
+++ src/components/AppComponent.jsx
@@ -1,30 +1,23 @@
 import React, { Component } from 'react';
-import ReactDOM, { render } from 'react-dom';
-import { Link } from 'react-router';
+
 import ControlPanel from './ControlPanel.jsx';
 import FolderTree from './FolderTree.jsx';
 import NotePanel from './NotePanel.jsx';
 import Folder from './Folder.jsx';
-import { createStore } from 'redux';
+
 import { connect } from 'react-redux';
-import { MAKE_FOLDER_ACTIVE } from '../actions/actionTypes.jsx';
 
 export class App extends Component {
     constructor(props) {
         super(props);
     }
 
-    // componentDidMount() {
-    //     debugger;
-    //     mapStateToProps(this.state);
-    // }
-
     render() {
         return (
             <div>
                 <ControlPanel />
                 <FolderTree />
-                <NotePanel notes={this.props.notes} />
+                <NotePanel notes={this.props.notes}  />
             </div>
         );
     }
@@ -36,7 +29,7 @@ const mapStateToProps = state => {
     }
 };
 
-const mapDispatchToProps = () => ({
+const mapDispatchToProps = dispatch => ({
 
 });
 
