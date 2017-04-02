1d741cedbb7a2b7236b62d581e77b6badcc43c0e
MacBook <jim.casey321@gmail.com>
2017-04-02 21:15:18 +0300

    Raise coverage of EditNote component

--- src/components/__tests__/EditNote.spec.js
+++ src/components/__tests__/EditNote.spec.js
@@ -1,9 +1,38 @@
+jest.mock('../../actions/EditNote.actions.jsx');
 import React, { Component } from 'react';
 import { shallow, mount } from 'enzyme';
-import EditNote from '../EditNote.jsx';
+import { EditNote, mapStateToProps, mapDispatchToProps } from '../EditNote.jsx';
+import {
+  editNoteRequest,
+  createNoteRequest
+} from '../../actions/EditNote.actions.jsx';
+import * as redux from 'redux';
+
 
 describe('EditNote tests', () => {
-    it('should return true', () => {
-        expect(true).toBe(true);
+    describe('#mapStateToProps', () => {
+        it('should populate props from state', () => {
+            const state = { params: {}, isNoteCreationMode: true, activeFolderId: 0 };
+            const props = {};
+
+            const mappedProps = mapStateToProps(state, props);
+
+            expect(mappedProps.folderName).toBe(state.folderName);
+        });
+    });
+
+    describe('#mapDispatchToProps', () => {
+        it('should map action creators to props', () => {
+            redux.bindActionCreators = jest.fn();
+            const dispatch = jest.fn();
+            const actionCreators = {
+              editNoteRequest,
+              createNoteRequest
+            };
+
+            const mappedDispatch = mapDispatchToProps(dispatch);
+
+            expect(redux.bindActionCreators).toHaveBeenCalledWith(actionCreators, dispatch);
+        });
     });
 });
