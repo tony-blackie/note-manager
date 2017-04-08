21b6c68607c4283cb9addc51da74d5ce54ba2a92
MacBook <jim.casey321@gmail.com>
2017-04-08 16:02:55 +0300

    Use fetch instead of jQuery for async actions

--- src/views/EditNote/actions/EditNote.actions.jsx
+++ src/views/EditNote/actions/EditNote.actions.jsx
@@ -10,29 +10,42 @@ import {
 export const editNoteRequest = note => dispatch => {
     dispatch({type: EDIT_EXISTING_NOTE});
 
-    return $.ajax({
-      url: `/notes/${note.id}`,
-      data: JSON.stringify(note),
-      contentType: 'application/json',
-      type: 'PUT'
-    }).then(response => {
+    return fetch(`/notes/${note.id}`, {
+        method: 'PUT',
+        body: JSON.stringify(note),
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        }
+    })
+    .then(response => {
         dispatch({type: EDIT_EXISTING_NOTE_SUCCESS, payload: response});
 
         hashHistory.push('/');
+    })
+    .catch(error => {
+        //dispatch error
     });
+
 }
 
 export const createNoteRequest = note => dispatch => {
     dispatch({type: CREATE_NEW_NOTE});
 
-    return $.ajax({
-      url: '/note',
-      data: JSON.stringify(note),
-      contentType: 'application/json',
-      type: 'POST'
-    }).then(response => {
-        dispatch({type: CREATE_NEW_NOTE_SUCCESS, payload: response});
+    return fetch(`/note`, {
+        method: 'POST',
+        body: JSON.stringify(note),
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        }
+    })
+    .then(response => {
+        dispatch({type: EDIT_EXISTING_NOTE_SUCCESS, payload: response});
 
         hashHistory.push('/');
+    })
+    .catch(error => {
+        //dispatch error
     });
 }
---
+++ src/views/EditNote/actions/__tests__/EditNote.actions.spec.js
@@ -0,0 +1,7 @@
+import * as actions from '../EditNote.actions.jsx';
+
+describe('EditNote actions', () => {
+    it('should return a thunk', () => {
+        expect(true).toEqual(true);
+    });
+});
