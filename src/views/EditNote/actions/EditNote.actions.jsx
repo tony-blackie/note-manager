4b51de74ef33b4a12d898b5ffdccf47b34e1e17c
MacBook <jim.casey321@gmail.com>
2017-04-09 12:15:37 +0300

    Raise coverage of editNote actions

--- src/views/EditNote/actions/EditNote.actions.jsx
+++ src/views/EditNote/actions/EditNote.actions.jsx
@@ -3,9 +3,22 @@ import { hashHistory } from 'react-router';
 import {
   EDIT_EXISTING_NOTE,
   EDIT_EXISTING_NOTE_SUCCESS,
+  EDIT_EXISTING_NOTE_FAIL,
   CREATE_NEW_NOTE,
   CREATE_NEW_NOTE_SUCCESS
 } from '../../actionTypes.jsx';
+
+export const handleEditNoteSuccess = response => {
+    hashHistory.push('/');
+
+    return ({type: EDIT_EXISTING_NOTE_SUCCESS, payload: response});
+}
+
+export const handleEditNoteFail = error => {
+    hashHistory.push('/');
+
+    return ({ type: EDIT_EXISTING_NOTE_FAIL, payload: error });
+}
 
 export const editNoteRequest = note => dispatch => {
     dispatch({type: EDIT_EXISTING_NOTE});
@@ -18,15 +31,8 @@ export const editNoteRequest = note => dispatch => {
             'Content-Type': 'application/json'
         }
     })
-    .then(response => {
-        dispatch({type: EDIT_EXISTING_NOTE_SUCCESS, payload: response});
-
-        hashHistory.push('/');
-    })
-    .catch(error => {
-        //dispatch error
-    });
-
+    .then(response => dispatch(handleEditNoteSuccess(response)))
+    .catch(error => dispatch(handleEditNoteFail(error)));
 }
 
 export const createNoteRequest = note => dispatch => {
--- src/views/EditNote/actions/__tests__/EditNote.actions.spec.js
+++ src/views/EditNote/actions/__tests__/EditNote.actions.spec.js
@@ -1,7 +1,35 @@
 import * as actions from '../EditNote.actions.jsx';
+import fetchMock from 'fetch-mock';
 
 describe('EditNote actions', () => {
-    it('should return a thunk', () => {
-        expect(true).toEqual(true);
+    it('should dispatch EDIT_EXISTING_NOTE', () => {
+        let dispatch = jest.fn(() => actions.handleEditNoteSuccess);
+        fetchMock.putOnce('*', { result: [] });
+
+        let dispatcher = actions.editNoteRequest(5);
+        dispatcher(dispatch);
+
+        expect(dispatch.mock.calls[0][0]).toEqual({ type: 'EDIT_EXISTING_NOTE' });
+    });
+
+    it('should dispatch EDIT_EXISTING_NOTE_SUCCESS', () => {
+        fetchMock.putOnce('*', { result: [] });
+        window.Headers = jest.fn();
+        let dispatch = jest.fn(() => actions.handleEditNoteSuccess);
+        let dispatcher = actions.editNoteRequest({id: 5});
+        return dispatcher(dispatch).then(response => {
+            expect(dispatch.mock.calls[1][0].type).toEqual('EDIT_EXISTING_NOTE_SUCCESS');
+        })
+    });
+
+    it('should dispatch EDIT_EXISTING_NOTE_FAIL', () => {
+        let mockError = { message: 'Server error' };
+        fetchMock.putOnce('*', { status: 503, throws: mockError });
+        window.Headers = jest.fn();
+        let dispatch = jest.fn(() => actions.handleEditNoteFail);
+        let dispatcher = actions.editNoteRequest({id: 5});
+        return dispatcher(dispatch).catch(response => {
+            expect(dispatch.mock.calls[1][0].type).toEqual('EDIT_EXISTING_NOTE_FAIL');
+        })
     });
 });
