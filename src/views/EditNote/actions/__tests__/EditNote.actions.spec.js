277ea2edb14f204315219a375f1ba7f896597edd
tony-blackie <jim.casey321@gmail.com>
2017-05-03 23:28:16 +0300

    Raise coverage of note.actions to 100%

--- src/views/EditNote/actions/__tests__/EditNote.actions.spec.js
+++ src/views/EditNote/actions/__tests__/EditNote.actions.spec.js
@@ -106,4 +106,48 @@ describe('EditNote actions', () => {
               .toEqual({ type: 'GET_NOTE_FAIL', error: {} });
         });
     });
+
+    describe('#clearNoteData', () => {
+        it('should dispatch with specific action type', () => {
+            let dispatch = jest.fn();
+            let dispatcher = actions.clearNoteData();
+            dispatcher(dispatch);
+            expect(dispatch)
+              .toHaveBeenCalledWith({ type: 'CLEAR_NOTE_DATA' });
+        });
+    });
+
+    describe('#fetchNote', () => {
+        it('should dispatch REQUEST_NOTE_FETCH', () => {
+            let dispatch = jest.fn(() => actions.handleSuccessfulNoteResponse);
+            fetchMock.getOnce('*', { result: [] });
+
+            let dispatcher = actions.fetchNote(5);
+            dispatcher(dispatch);
+
+            expect(dispatch.mock.calls[0][0]).toEqual({ type: 'REQUEST_NOTE_FETCH' });
+        });
+
+        it('should dispatch CREATE_NEW_NOTE_SUCCESS', () => {
+            fetchMock.getOnce('*', { result: [] });
+            window.Headers = jest.fn();
+            let dispatch = jest.fn(() => actions.handleSuccessfulNoteResponse);
+            let dispatcher = actions.fetchNote(5);
+            return dispatcher(dispatch).then(response => {
+                expect(dispatch.mock.calls[1][0].type).toEqual('GET_NOTE_SUCCESS');
+            })
+        });
+
+        it('should dispatch CREATE_NEW_NOTE_FAIL', () => {
+            let mockError = { message: 'Server error' };
+            fetchMock.getOnce('*', { status: 503, throws: mockError });
+            window.Headers = jest.fn();
+            let dispatch = jest.fn(() => actions.handleFailedNoteResponse);
+            let dispatcher = actions.fetchNote(5);
+            return dispatcher(dispatch).catch(response => {
+                return console.log(dispatch.mock.calls[1][0].type);
+                expect(dispatch.mock.calls[1][0].type).toEqual('CREATE_NEW_NOTE_FAIL');
+            })
+        });
+    });
 });
