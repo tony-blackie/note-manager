0ee86e9a62bc2c8970aff20f87073c3ee836da29
Tony Spaskyi <jim.casey321@gmail.com>
2017-10-10 12:49:35 +0300

    Add function types

--- src/views/EditFolder/types.ts
+++ src/views/EditFolder/types.ts
@@ -1,5 +1,8 @@
+import { TypedAction } from '../../generic/types';
+
 export interface EditFolderState {
     folderName: string;
+    folderId: number | null;
 }
 
 export type GetFolderFn = (id: string) => void;
@@ -9,3 +12,5 @@ export type CreateNewFolderFn = (folderName: string) => void;
 export type EditFolderFn = (id: number, name: string) => void;
 
 export type HandleFolderNameChangeFn = (text: string) => void;
+
+export type HandleSuccessfulFolderEditFn = (folderId: number | null, folderName: string) => TypedAction;
