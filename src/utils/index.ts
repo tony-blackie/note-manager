067ad2da6ea26d02ba7bfd6e15c1524f8106d256
Tony Spaskyi <jim.casey321@gmail.com>
2017-10-16 18:07:56 +0300

    Add utils for using auth headers and localstorage

---
+++ src/utils/index.ts
@@ -0,0 +1,9 @@
+import { setDefaultAuthHeader } from './setDefaultAuthHeader';
+import { setToken } from './setToken';
+import { getToken } from './getToken';
+
+export default {
+    setDefaultAuthHeader,
+    setToken,
+    getToken
+};
