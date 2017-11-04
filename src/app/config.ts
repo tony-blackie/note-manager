4bd367eec8f401d2ff6b4946065b042b09c56ccf
Tony Spaskyi <jim.casey321@gmail.com>
2017-11-04 09:37:43 +0200

    change config and add hardcoded working user

--- src/app/config.ts
+++ src/app/config.ts
@@ -1,5 +1,5 @@
 export const baseName = 'http://localhost:8000';
 
-export const clientID = 'jJzbQf5LBuVpE7R2ehDLPLH4dXT0TFIq6PsFuUTx';
+export const clientID = 'VQRURUkxcSARIHW89k4TjEq0ilFbfJS8eCeoUrTy';
 
-export const clientSecret = '9BR8y0PdKyhLzXDeUand5td4kwjaAjhxIMDHLhPXQOH7BsmCLzEvzVL7QaZj82sb0sEIWrc9pBp4q8iX6Q5kVKf4soGcKQAJfF2BIxHHuA3hYJWr14gQbA9cewg2It4K';
+export const clientSecret = '1DVtFQLvAqj4VYaf0pXihSt2igldTdBnOmBY4rQ9arWN1uodaGTdNFtRlRLR0DJAEhqjKKnZkwcGEhz42DgPFNQhvfBSGrTfIxv9A2VEukXHmThNMmWok6OqMEVQbAkR';
--- src/views/Login/actions.ts
+++ src/views/Login/actions.ts
@@ -8,20 +8,24 @@ import { hashHistory } from 'react-router';
  const { setToken } = utils;
 
 export const requestToken = (username, password) => dispatch => {
+    username = 'tony';
+    password = 'admin123';
     const grant_type = 'password';
     const client_id = clientID;
     const client_secret = clientSecret;
 
-    return axios.request({
-        url: `${baseName}/o/token/`,
-        method: 'POST',
-        params: {
+    const params = {
             grant_type,
             username,
             password,
             client_secret,
             client_id
-        }
+    };
+
+    return axios.request({
+        url: `${baseName}/o/token/`,
+        method: 'POST',
+        params
     })
     .then(response => {
         const { token_type, access_token } = response.data;
