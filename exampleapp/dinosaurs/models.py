90d6b5f1884668598d3c047a3b9ae9771875e771
Tony Spaskyi <jim.casey321@gmail.com>
2017-11-07 23:35:22 +0200

    Fix problems with required dependencies

--- exampleapp/dinosaurs/models.py
+++ exampleapp/dinosaurs/models.py
@@ -15,9 +15,9 @@ class Person(User):
 
 class Folder(models.Model):
     name = models.TextField()
-    parent = models.IntegerField(null=True)
-    author = models.ForeignKey(Person, related_name='folders', on_delete=models.CASCADE,)
-    is_root = models.BooleanField()
+    parent = models.IntegerField(null=True, blank=True)
+    author = models.ForeignKey(Person, null=True, blank=True, related_name='folders', on_delete=models.CASCADE,)
+    is_root = models.BooleanField(blank=True)
 
     def __str__(self):
         return str(self.name)
@@ -26,8 +26,8 @@ class Note(models.Model):
     # parent = models.IntegerField(null=True)
     name = models.TextField()
     text = models.TextField()
-    folder = models.ForeignKey(Folder, null=True, related_name='notes', on_delete=models.CASCADE,)
-    author = models.ForeignKey(Person, related_name='notes', on_delete=models.CASCADE,)
+    folder = models.ForeignKey(Folder, null=True, blank=True, related_name='notes', on_delete=models.CASCADE,)
+    author = models.ForeignKey(Person, null=True, blank=True, related_name='notes', on_delete=models.CASCADE,)
 
     def __str__(self):
         return str(self.name)
