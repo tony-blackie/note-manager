224b8f3f282f67e9452f72081163541fe9b972ec
Tony Spaskyi <jim.casey321@gmail.com>
2017-11-12 00:46:23 +0200

    Change renderer for folder API, so that it would be possible to serve both JSON data and html from a single endpoint

--- exampleapp/dinosaurs/admin.py
+++ exampleapp/dinosaurs/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from .models import User, Note, Folder
+from .models import User, Note, Folder, Person
 
-# admin.site.register(Person)
+admin.site.register(Person)
 admin.site.register(Folder)
 admin.site.register(Note)
--- exampleapp/dinosaurs/views.py
+++ exampleapp/dinosaurs/views.py
@@ -16,6 +16,12 @@ from rest_framework import exceptions
 from django.contrib.auth.hashers import make_password
 from django.contrib.auth import get_user_model
 import re
+from rest_framework.response import Response
+from rest_framework.renderers import BrowsableAPIRenderer, JSONRenderer
+
+class CustomBrowsableAPIRenderer(BrowsableAPIRenderer):
+    def get_default_renderer(self, view):
+        return JSONRenderer()
 
 class PersonAPIView(APIView):
     permission_classes = [permissions.AllowAny]
@@ -49,28 +55,35 @@ class PersonAPIView(APIView):
         return HttpResponse(json.dumps(serializer.data))
 
 class FolderAPIView(APIView):
-    queryset = Folder.objects.all()
     permission_classes = [permissions.AllowAny]
+    serializer_class = FolderSerializer
+    queryset = Folder.objects.all()
+    renderer_classes = (CustomBrowsableAPIRenderer,)
 
     def get(self, request):
         # pdb.set_trace()
 
+        # if request.user.is_staff:
+        # folders = Folder.objects.all()
+        # serializer = FolderSerializer(folders, many=True)
+        # return Response(serializer.data)
+
         # Fix admin panel for folders
 
-        if re.match(r'/folder/$', request.path):
+        if re.search(r'/folder/$', request.path):
             folders = Folder.objects.all()
-            serializer = FolderSerializer(folders)
-            return HttpResponse(json.dumps(serializer.data))
+            serializer = FolderSerializer(folders, many=True)
+            return Response(serializer.data)
 
         userId = request.user.id
 
-        try:
-            folders = Folder.objects.get(author=userId)
-        except Folder.DoesNotExist:
-            return HttpResponse(json.dumps([]))
+        # try:
+        #     folders = Folder.objects.get(author=userId)
+        # except Folder.DoesNotExist:
+        #     return HttpResponse(json.dumps([]))
 
-        serializer = FolderSerializer(Folder.objects.filter(author = request.user.id), many=True)
-        return HttpResponse(json.dumps(serializer.data))
+        # serializer = FolderSerializer(Folder.objects.filter(author = request.user.id), many=True)
+        # return HttpResponse(json.dumps(serializer.data))
 
     def post(self, request):
         userId = request.user.id
