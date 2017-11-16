5b8c3b79a1b5ced70963a21171f5513319f5ed38
Tony Spaskyi <jim.casey321@gmail.com>
2017-11-16 21:19:17 +0200

    Add Note POST API for creating new notes

--- exampleapp/dinosaurs/views.py
+++ exampleapp/dinosaurs/views.py
@@ -127,17 +127,99 @@ class FolderAPIView(APIView):
         return Response(serializer.data)
 
     def delete(self, request):
+        # TODO: Remove notes associated with deleted folder, if it's not empty
         userId = request.data.user
         serializer = FolderSerializer(Folder.objects.filter(author = request.user.id), many=True)
         return Response(serializer.data)
 
-class NoteViewSet(viewsets.ModelViewSet):
+class NoteAPIView(APIView):
+    permission_classes = [permissions.AllowAny]
     serializer_class = NoteSerializer
     queryset = Note.objects.all()
 
-    def list(self, request):
-        serializer = NoteSerializer(Note.objects.filter(author = request.user.id), many=True)
-        return HttpResponse(json.dumps(serializer.data))
+    def get(self, request, id = None):
+        def remove_slashes(string):
+            return string.replace('/', '')
+
+        if id == '':
+            if re.search(r'admin/dinosaurs/note/$', request.path):
+                notes = Note.objects.all()
+                serializer = NoteSerializer(notes, many=True)
+                return Response(serializer.data)
+
+            if re.search(r'/note/$', request.path):
+                userId = request.user.id
+
+                try:
+                    notes = Note.objects.filter(author=userId)
+                except Note.DoesNotExist:
+                    return Response([])
+
+                serializer = NoteSerializer(Note.objects.filter(author = request.user.id), many=True)
+
+                return Response(serializer.data)
+
+            if re.search(r'/note/', request.path):
+                userId = request.user.id
+
+                try:
+                    notes = Note.objects.filter(author=userId)
+                except Note.DoesNotExist:
+                    return Response([])
+
+                serializer = NoteSerializer(Note.objects.filter(author = request.user.id), many=True)
+
+                return Response(serializer.data)
+        else:
+            id = int(remove_slashes(id))
+            userId = request.user.id
+            serializer = NoteSerializer(Note.objects.get(author = request.user.id, id = id))
+            return Response(serializer.data)
+
+    def post(self, request, id = None):
+        userId = request.user.id
+        parentFolderId = request.data.get('folder', None)
+
+        if parentFolderId:
+            parentFolder = Folder.objects.get(id=int(parentFolderId))
+        else:
+            foldersForCurrentUser = Folder.objects.filter(author=userId)
+            for folder in foldersForCurrentUser:
+                if folder.is_root == True:
+                    parentFolder = folder
+
+        note = Note.objects.create(
+            name = request.data.get('name', 'newName'),
+            text = request.data.get('text', 'defaultText'),
+            folder = parentFolder,
+            author = request.user
+        )
+
+        serializer = NoteSerializer(note)
+        return Response(serializer.data)
+
+    def put(self, request, id = None):
+        userId = request.user.id
+        folder = Folder.objects.get(id = userId, author = request.user.id)
+
+        folder.name = request.data.get('name')
+        folder.save()
+
+        serializer = FolderSerializer(folder)
+        return Response(serializer.data)
+
+    def delete(self, request):
+        userId = request.data.user
+        serializer = FolderSerializer(Folder.objects.filter(author = request.user.id), many=True)
+        return Response(serializer.data)
+
+# class NoteViewSet(viewsets.ModelViewSet):
+#     serializer_class = NoteSerializer
+#     queryset = Note.objects.all()
+
+#     def list(self, request):
+#         serializer = NoteSerializer(Note.objects.filter(author = request.user.id), many=True)
+#         return HttpResponse(json.dumps(serializer.data))
 
 class GroupViewSet(viewsets.ModelViewSet):
     permission_classes = [permissions.IsAuthenticated, TokenHasScope]
--- exampleapp/exampleapp/urls.py
+++ exampleapp/exampleapp/urls.py
@@ -25,13 +25,14 @@ from oauth2_provider.contrib.rest_framework import TokenHasReadWriteScope, Token
 
 router = routers.DefaultRouter()
 # router.register(r'folders/', views.AdminFoldersViewset)
-router.register(r'note', views.NoteViewSet)
+# router.register(r'note', views.NoteViewSet)
 router.register(r'groups', views.GroupViewSet)
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
     url(r'folder/(?P<id>.*)', views.FolderAPIView.as_view()),
+    url(r'note/(?P<id>.*)', views.NoteAPIView.as_view()),
     url(r'^users/', views.PersonAPIView.as_view()),
     url(r'^', include(router.urls)),
     url(r'^admin/', admin.site.urls),
