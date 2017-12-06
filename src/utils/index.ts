6d877e5a823cae62ee6a05be2408a6fe2418a28f
Tony Spaskyi <jim.casey321@gmail.com>
2017-12-06 08:04:03 +0200

    Add new util deleteToken

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,9 +1,11 @@
 import { setDefaultAuthHeader } from './setDefaultAuthHeader';
 import { setToken } from './setToken';
 import { getToken } from './getToken';
+import deleteToken from './deleteToken';
 
 export default {
     setDefaultAuthHeader,
     setToken,
-    getToken
+    getToken,
+    deleteToken
 };
