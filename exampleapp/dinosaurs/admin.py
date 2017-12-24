9c0941f59087e52e60ebb6cf32917e78c55f02f1
Tony Spaskyi <jim.casey321@gmail.com>
2017-12-24 21:02:50 +0200

    Remove backend from this repo

--- exampleapp/dinosaurs/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import User, Note, Folder, Person, Questionnaire
-
-admin.site.register(Person)
-admin.site.register(Folder)
-admin.site.register(Note)
-admin.site.register(Questionnaire)
--- exampleapp/dinosaurs/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class DinosaursConfig(AppConfig):
-    name = 'dinosaurs'
